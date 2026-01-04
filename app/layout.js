265804e202d2c6900bc6fe887ac1bbaa9ebb19cb
Next gencreations <darran.lounds@hotmail.com>
2026-01-04 07:35:39 +0000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="pip-crt">
+          <div className="pip-shell">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
