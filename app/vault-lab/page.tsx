ccd29530bdd93a4a3cc19d5bf68e7cf24ddfb723
Next gencreations <darran.lounds@hotmail.com>
2026-05-05 09:12:14 +0100

    Update page.tsx

--- app/vault-lab/page.tsx
+++ app/vault-lab/page.tsx
@@ -2,14 +2,30 @@
 
 import dynamic from "next/dynamic";
 
-const Game = dynamic(() => import("../../game/Game"), {
-  ssr: false,
-});
+const Game = dynamic(
+  () => import("../../game/Game").then((mod) => mod.Game),
+  {
+    ssr: false,
+    loading: () => (
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          background: "#010801",
+          color: "#00ff44",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          fontFamily: "'Courier New', monospace",
+          letterSpacing: 4,
+        }}
+      >
+        LOADING VAULT 63 LAB...
+      </div>
+    ),
+  }
+);
 
 export default function VaultLabPage() {
-  return (
-    <div style={{ width: "100vw", height: "100vh", background: "black" }}>
-      <Game />
-    </div>
-  );
+  return <Game />;
 }
