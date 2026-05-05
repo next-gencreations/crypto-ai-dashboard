0c11a699c5ba583ca41f0d8327d0e3a3ea49934e
Next gencreations <darran.lounds@hotmail.com>
2026-05-05 13:20:44 +0100

    Update page.tsx

--- app/vault-lab/page.tsx
+++ app/vault-lab/page.tsx
@@ -1,31 +1,17 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { WebGLErrorBoundary } from "../../game/WebGLCheck";
 
 const Game = dynamic(
   () => import("../../game/Game").then((mod) => mod.Game),
-  {
-    ssr: false,
-    loading: () => (
-      <div
-        style={{
-          width: "100vw",
-          height: "100vh",
-          background: "#010801",
-          color: "#00ff44",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          fontFamily: "'Courier New', monospace",
-          letterSpacing: 4,
-        }}
-      >
-        LOADING VAULT 63 LAB...
-      </div>
-    ),
-  }
+  { ssr: false }
 );
 
 export default function VaultLabPage() {
-  return <Game />;
+  return (
+    <WebGLErrorBoundary>
+      <Game />
+    </WebGLErrorBoundary>
+  );
 }
